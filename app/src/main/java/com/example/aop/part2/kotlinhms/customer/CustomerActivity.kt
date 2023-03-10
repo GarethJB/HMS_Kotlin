5cb2af1617ea9be3d280f08ecb44e9ca4061c112
JB <108816562+GarethJB@users.noreply.github.com>
2023-03-10 19:57:12 +0900

    update

---
+++ app/src/main/java/com/example/aop/part2/kotlinhms/customer/CustomerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aop.part2.kotlinhms.customer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aop.part2.kotlinhms.R
+
+class CustomerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer)
+    }
+}
---
+++ app/src/main/java/com/example/aop/part2/kotlinhms/customer/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.aop.part2.kotlinhms.customer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.aop.part2.kotlinhms.R
+
+
+class HomeFragment : Fragment() {
+
+
+}
